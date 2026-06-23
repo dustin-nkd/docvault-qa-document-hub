89260d66d915d288800aebced0badeba9ff7f53a
dustin-nkd <dustin-nkd@users.noreply.github.com>
2026-06-23 15:26:28 +0000

    deploy: 4e83e8b6941dc09d6f66249b015142fab5921a09

--- docvault.js
+++ docvault.js
@@ -878,6 +878,26 @@ function renderContent() {
                 initialValue: initialVal,
                 theme: 'dark'
             });
+            
+            // Inject copy buttons into code blocks
+            container.querySelectorAll('pre').forEach(pre => {
+                pre.style.position = 'relative';
+                const codeEl = pre.querySelector('code');
+                const textToCopy = codeEl ? codeEl.innerText : pre.innerText;
+                
+                const btn = document.createElement('button');
+                btn.className = 'code-copy-btn';
+                btn.title = 'Copy';
+                btn.innerHTML = '<i class="fa-regular fa-copy"></i>';
+                btn.onclick = function(e) {
+                    e.stopPropagation();
+                    navigator.clipboard.writeText(textToCopy).then(() => {
+                        btn.innerHTML = '<i class="fa-solid fa-check"></i>';
+                        setTimeout(() => btn.innerHTML = '<i class="fa-regular fa-copy"></i>', 2000);
+                    });
+                };
+                pre.appendChild(btn);
+            });
         }
     }
 }
